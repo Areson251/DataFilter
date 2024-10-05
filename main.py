d7585a9e168b26b369e47ad65f12a5b36b731cff
Areson251 <unicorn5596@gmail.com>
2024-10-05 17:54:35 +0400

    mvp

--- main.py
+++ main.py
@@ -1,54 +1,182 @@
 import os
 import argparse
 from pycocotools.coco import COCO
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
 import numpy as np
 import random
+import json
 
-class DataFilter():
+seed = 0
+random.seed(seed)
+np.random.seed(seed)
+
+IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp')
+
+
+class DataFilter(QtWidgets.QWidget):
     def __init__(self, images_path, annotation_path, output_path) -> None:
+        super().__init__()
         self.images_path = images_path
         self.annotation_path = annotation_path
         self.output_path = output_path
 
         self.annotation = None
         self.new_annotation = {}
+        self.images_paths = []
+        self.current_image_index = 0
+        self.total_images = 0
+
+        self.colors_count = 0
+        self.colors = []
 
         self.app = None
-        self.window = None
 
         self.check_folders()
         self.read_annotation()
         self.load_app()
+        self.setup()
 
-        self.filter()
+        self.init_ui()
+        self.show_image()
+
         self.stop_app()
 
-    def filter(self):
-        images_paths = os.listdir(self.images_path)
-        print(f"Total images: {len(images_paths)}")
+    def init_ui(self):
+        self.setWindowTitle('PiVAT')
 
-        for id, image_file in enumerate(images_paths):
-            image_id = id
-            image_path = os.path.join(self.images_path, image_file)
-            image = Image.open(image_path).convert("RGB")
-            
-            annotation_ids = self.annotation.getAnnIds(imgIds=image_id)
-            annotations = self.annotation.loadAnns(annotation_ids)
+        # Layout for the window
+        self.layout = QtWidgets.QHBoxLayout()
 
-            draw = ImageDraw.Draw(image)
-            for ann in annotations:
-                mask = self.annotation.annToMask(ann)
-                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # Random color for each mask
+        # Control images
+        self.image_layout = QtWidgets.QHBoxLayout()
+        self.original_image_label = QtWidgets.QLabel()
+        self.masked_image_label = QtWidgets.QLabel()
 
-                for i in range(mask.shape[0]):
-                    for j in range(mask.shape[1]):
-                        if mask[i, j] == 1:
-                            image.putpixel((j, i), color)
+        self.image_layout.addWidget(self.original_image_label)
+        self.image_layout.addWidget(self.masked_image_label)
 
-            self.display_image_with_masks(image)
+        self.layout.addLayout(self.image_layout)
+
+        # Control buttons
+        self.button_layout = QtWidgets.QVBoxLayout()
+        self.button_layout.setContentsMargins(0, 0, 0, 0)
+        self.button_layout.setSpacing(1)
+
+        self.prev_button = QtWidgets.QPushButton("<- Previous")
+        self.prev_button.clicked.connect(self.show_previous_image)
+        self.prev_button.setFixedSize(120, 30)
+        self.button_layout.addWidget(self.prev_button)
+
+        self.next_button = QtWidgets.QPushButton("Next ->")
+        self.next_button.clicked.connect(self.show_next_image)
+        self.next_button.setFixedSize(120, 30)
+        self.button_layout.addWidget(self.next_button)
+
+        self.save_next_button = QtWidgets.QPushButton("Save and next")
+        self.save_next_button.clicked.connect(self.save_image)
+        self.save_next_button.setFixedSize(120, 30)
+        self.button_layout.addWidget(self.save_next_button)
+
+        self.save_annotation_button = QtWidgets.QPushButton("Save annotation")
+        self.save_annotation_button.clicked.connect(self.save_annotation)
+        self.save_annotation_button.setFixedSize(120, 30)
+        self.button_layout.addWidget(self.save_annotation_button)
+
+        self.image_id_label = QtWidgets.QLabel(f"Current image id: {self.current_image_index}")
+        self.button_layout.addWidget(self.image_id_label)
+
+        # Connect widgets to the layout
+        self.layout.addLayout(self.button_layout)
+
+        self.setLayout(self.layout)
+        self.show()
+
+    def show_image(self):
+        image_id = self.current_image_index  # Используем индекс как ID
+        self.image_info = self.annotation.loadImgs(image_id)
+
+        self.image_path = os.path.join(self.images_path, self.image_info[0]['file_name'])
+        self.image = Image.open(self.image_path).convert("RGB")
+
+        # Open original image
+        original_image = Image.open(self.image_path).convert("RGB")
+        pixmap = self.display_image(original_image)
+        self.original_image_label.setPixmap(pixmap)
+        
+        annotation_ids = self.annotation.getAnnIds(imgIds=image_id)
+        annotations = self.annotation.loadAnns(annotation_ids)
+
+        draw = ImageDraw.Draw(self.image)
+        for ann in annotations:
+            mask = self.annotation.annToMask(ann)
+            color = self.colors[ann["category_id"]]
+            text = self.annotation.loadCats(ann["category_id"])[0]["name"]
+
+            for i in range(mask.shape[0]):
+                for j in range(mask.shape[1]):
+                    if mask[i, j] == 1:
+                        self.image.putpixel((j, i), color)
+
+            draw.text((ann["bbox"][0], ann["bbox"][1]), text, fill=color, font=self.font)
+
+        pixmap = self.display_image(self.image)
+        self.masked_image_label.setPixmap(pixmap)
+
+        self.image_id_label.setText(f"Current image id: {self.current_image_index}")
+
+
+    def display_image(self, image):
+        """Display the original image on the left side."""
+        image = image.convert("RGBA")
+        data = image.tobytes("raw", "RGBA")
+        qim = QtGui.QImage(data, image.width, image.height, QtGui.QImage.Format_RGBA8888)
+        pixmap = QtGui.QPixmap.fromImage(qim)
+
+        return pixmap
+
+    def show_next_image(self):
+        if self.current_image_index < self.total_images - 1:
+            self.current_image_index += 1
+        else:
+            self.current_image_index = 0
+        self.show_image()
+
+    def show_previous_image(self):
+        if self.current_image_index > 0:
+            self.current_image_index -= 1
+        else:
+            self.current_image_index = self.total_images - 1
+        self.show_image()
+
+    def save_image(self):
+        image_id = self.current_image_index
+        image_info = self.annotation.loadImgs(image_id)
+
+        image_path = os.path.join(self.images_path, image_info[0]['file_name'])
+        image = Image.open(image_path).convert("RGB")
+
+        annotation_ids = self.annotation.getAnnIds(imgIds=image_id)
+        annotations = self.annotation.loadAnns(annotation_ids)
+
+        categories_ids = {ann["category_id"] for ann in annotations}
+        categories = self.annotation.loadCats(categories_ids)
+
+        self.new_annotation["images"].append(image_info[0])
+        for ann in annotations:
+            self.new_annotation["annotations"].append(ann)
+
+        for category in categories:
+            if category not in self.new_annotation["categories"]:
+                self.new_annotation["categories"].append(category)
+    
+        image.save(os.path.join(self.output_path, image_info[0]['file_name']))
+
+        # Next image
+        if self.current_image_index < self.total_images - 1:
+            self.current_image_index += 1
+            self.show_image()
 
     def check_folders(self):
         if not os.path.exists(self.images_path):
@@ -66,24 +194,50 @@ class DataFilter():
         self.new_annotation["images"] = []
         self.new_annotation["annotations"] = []
 
+        # only images files
+        self.images_paths = [f for f in os.listdir(self.images_path) 
+                             if f.lower().endswith(IMAGE_EXTENSIONS)]
+        self.total_images = len(self.images_paths)
+        print(f"Total images: {self.total_images}")
+
+    def fix_idxs(self):
+        for idx_cat, cat in enumerate(self.new_annotation["categories"]):
+            old_id_cat = cat["id"]
+            cat["id"] = idx_cat
+            
+            for idx_omg, img in enumerate(self.new_annotation["images"]):
+                old_id_img = img["id"]
+                img["id"] = idx_omg
+
+                for ann in self.new_annotation["annotations"]:
+                    if ann["category_id"] == old_id_cat:
+                        ann["category_id"] = idx_cat
+                    if ann["image_id"] == old_id_img:
+                        ann["image_id"] = idx_omg
+
+    def save_annotation(self):
+        self.fix_idxs()
+        with open(os.path.join(self.output_path, "annotation.json"), "w") as f:
+            json.dump(self.new_annotation, f)
+
+    def setup(self):
+        self.colors_count = len(self.annotation.dataset["categories"])
+        self.colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in range(self.colors_count)]
+        self.font = ImageFont.load_default(size=15)
+
+    def keyPressEvent(self, event):
+        """Handle key press events"""
+        if event.key() == QtCore.Qt.Key_D:
+            self.show_next_image()
+        elif event.key() == QtCore.Qt.Key_A:
+            self.show_previous_image()
+        elif event.key() == QtCore.Qt.Key_S:
+            self.save_image()
+        elif event.key() == QtCore.Qt.Key_W:
+            self.save_annotation()
+
     def load_app(self):
         self.app = QtWidgets.QApplication(sys.argv)
-        self.window = QtWidgets.QWidget()
-
-    def display_image_with_masks(self, image):
-        image = image.convert("RGBA")
-        data = image.tobytes("raw", "RGBA")
-        qim = QtGui.QImage(data, image.width, image.height, QtGui.QImage.Format_RGBA8888)
-        pixmap = QtGui.QPixmap.fromImage(qim)
-
-        label = QtWidgets.QLabel()
-        label.setPixmap(pixmap)
-
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(label)
-        self.window.setLayout(layout)
-        self.window.setWindowTitle('Image with Masks')
-        self.window.show()
 
     def stop_app(self):
         sys.exit(self.app.exec_())
@@ -96,4 +250,6 @@ if __name__ == "__main__":
     parser.add_argument("--output_path", type=str, required=True)
     args = parser.parse_args()
 
+    app = QtWidgets.QApplication(sys.argv)
     filter = DataFilter(args.images_path, args.annotation_path, args.output_path)
+    sys.exit(app.exec_())
